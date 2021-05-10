efe0608cc27bc1b80bd2683bd2e7704c910fd0bc
ivogui <sunjiayi@yandex.com>
2021-05-11 00:52:09 +0800

    booksmallupdata2021-5-11

--- zikaow/Page/booksmall.py
+++ zikaow/Page/booksmall.py
@@ -3,19 +3,16 @@ import requests
 
 
 class BooksMall(BasePage):
-    bookList = []
-    Bookinformation = []
-    ConfirmOrder = {}
 
     def Books_Mall(self):  # 进入书籍商城
         self.steps('../TestData/booksmall.yml', 'Books_Mall')
 
-    def slide(self):
+    def slide(self):  # 暂时无用
         self.steps('../TestData/booksmall.yml', 'Books_Mall')
         while True:
             if self.isElementPresent("xpath", "//*[@class='android.widget.FrameLayout' "
                                               "and @index='1']") is False:
-                self.steps('../TestData/booksmall.yml', 'bookList')
+                self.steps('../TestData/booksmall.yml', 'slide')
             elif self.isElementPresent("xpath", "//*[@class='android.widget.FrameLayout' "
                                                 "and @index='1']") is True:
                 pass
@@ -23,27 +20,32 @@ class BooksMall(BasePage):
             elif self.isElementPresent("id", "com.zikao.eduol:id/bottom_line_tv") is True:
                 break
 
-        # self.bookDetails = self.steps('../TestData/booksmall.yml', 'bookDetails')
-        # self.ConfirmOrder = self.steps('../TestData/booksmall.yml', 'ConfirmOrder')
-
     def books_back(self):  # 返回操作
         self.steps('../TestData/booksmall.yml', 'books_back')
 
-    def get_book_list(self):
+    def get_book_list(self):  # 通过requests.get获取接口.json数据
         a = requests.get('https://tk.360xkw.com/crgk/app/shop/getShopProductList?',
                          params={'courseId': '491', 'keyWord': '', 'sort': '0', 'topOrDown': 'false',
                                  'majorId': '0', 'subCourseId': '0', 'pageCurrent': '1', 'pageSize': '254'})
-        return a
+        return a.json()
 
-    def get_book_element(self, pos, info):
-        s1 = "//*[@class='android.widget.FrameLayout' and @index='%s']" % pos
-        s2 = "%s" % info
-        element = self.find("xpath", f'{s1}{s2}')
-        return element
+    def get_value(self, pos, text):  # 筛选接口数据中具体书本的字典表
+        # 获取接口数据中的data数据中的records数据列表
+        value1 = self.get_book_list().get('data').get('records')
+        # 便利records列表，获得具体书本的字典表
+        value2 = [d for d in value1 if d['name'] == self.get_book_element(pos, text)][0]
+        return value2
 
-    def get_pos(self):
-        pass
+    def get_book_element(self, pos, text):  # 拼接位置和书本信息，获取书本文本信息
+        element1 = self._driver.find_element_by_xpath(f'{self.get_pos(pos)}{self.get_info(text)}')\
+            .get_attribute("text")
+        return element1
 
-    def get_info(self, path):
-        pass
+    def get_pos(self, pos):  # 书城位置xpath，通过pos传递位置坐标
+        pos1 = "//*[@class='android.widget.FrameLayout' and @index='%s']" % pos
+        return pos1
+
+    def get_info(self, text):  # 书属性xpath，通过text参数传递获取的属性
+        info1 = "//*[@resource-id='com.zikao.eduol:id/item_book_%s']" % text
+        return info1
 
--- zikaow/TestCase/test_booksmall.py
+++ zikaow/TestCase/test_booksmall.py
@@ -1,4 +1,5 @@
 from zikaow.Page.app import App
+import pytest
 
 
 class TestLogin:
@@ -11,13 +12,37 @@ class TestLogin:
     def test_go_booksMall(self):
         self.BooksMall.Books_Mall()
 
-    def test_get_book_list(self):
-        self.BooksMall.slide()
+    @pytest.mark.parametrize("pos", [pos for pos in range(1, 10)])
+    def test_get_book_list(self, pos):
+        while True:
+            if self.testDriver.isElementPresent("id", "com.zikao.eduol:id/bottom_line_tv") is True:
+                break
+            elif self.testDriver.isElementPresent('xpath', self.BooksMall.get_pos(pos)) is False:
+                self.testDriver.steps('../TestData/booksmall.yml', 'slide')
+            else:
+                print("--------------a---------------")
+                aa = self.BooksMall.get_book_element(pos, "title")
+                print(aa)
+                bb = self.BooksMall.get_book_element(pos, "hint")
+                print(bb)
+                cc = self.BooksMall.get_book_element(pos, "price")
+                print(cc)
+                dd = self.BooksMall.get_book_element(pos, "sales")
+                print(dd)
+                print("--------------b---------------")
+                dict1 = self.BooksMall.get_value(pos, 'title')
+                print(dict1)
+                a = dict1['name']
+                print(a)
+                b = dict1['discountPrice']
+                print(b)
+                c = dict1['sales']
+                print(c)
+                d = dict1['briefIntroduction']
+                print(d)
 
-
-    def teardown(self):
-        self.BooksMall.books_back()
+    # def teardown(self):
+    #     self.BooksMall.books_back()
 
     def teardown_class(self):
         self.testDriver.quit()
-
